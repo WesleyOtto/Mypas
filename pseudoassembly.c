e021b7b043e2a89179d659d8dddbabb8eefbdfac
Lucas Pinheiro <pinheiro.lucasaugusto@gmail.com>
2016-11-29 23:18:16 -0200

    Refactor pseudoassembly instructions to receive the type as a parameter

--- pseudoassembly.c
+++ pseudoassembly.c
@@ -1,174 +1,198 @@
 
 int labelcounter = 1;
 
+// control pseudo-instructions
+
 int go_false(int label) {
-	fprintf(object, "\tjz .L%d\n", label);
-	return label;
+  fprintf(object, "\tjz .L%d\n", label);
+  return label;
 }
 
 int jump(int label) {
-	fprintf(object, "\tjmp .L%d\n", label);
-	return label;
+  fprintf(object, "\tjmp .L%d\n", label);
+  return label;
 }
 
 int mklabel(int label) {
-	fprintf(object, "\t.L%d:\n", label);
-	return label;
+  fprintf(object, "\t.L%d:\n", label);
+  return label;
 }
 
 int lmove_int(char const* variable) { //32 bits
-	fprintf (object, "\tmovl %%eax, %s\n", variable);
-	return 0
+  fprintf (object, "\tmovl %%rax, %s\n", variable);
+  return 0
 }
 
 int lmove_q(char const* variable) { //64 bits
-	fprintf (object, "\tmovq %%rax, %s\n", variable);
-	return 0;
+  fprintf (object, "\tmovq %%rax, %s\n", variable);
+  return 0;
 }
 
 int rmove_int(char const* variable) { //32 bits
-	fprintf(object, "\tpushl %%eax");
-	fprintf (object, "\tmovl %s, %%eax\n", variable);
-	return 0;
+  fprintf(object, "\tpushl %%rax");
+  fprintf (object, "\tmovl %s, %%rax\n", variable);
+  return 0;
 }
 
 int rmove_q(char const* variable) { //64 bits
-	fprintf(object, "\tpushl %%rax");
-	fprintf (object, "\tmovq %s, %%rax\n", variable);
-	return 0;
+  fprintf(object, "\tpushl %%rax");
+  fprintf (object, "\tmovq %s, %%rax\n", variable);
+  return 0;
 }
 
-/*ULA pseudo-instructions*/
-/*unary*/
-int neg_logic(void) {
-	//NOT
+// ULA pseudo-instructions
+
+// Unary instructions
+
+int neg_instruction(int type) {
+  switch(type) {
+    case BOOLEAN:
+      // TODO
+      break;
+    case INTEGER:
+      fprintf(obejct, "\tnegl %%rax\n");
+      break;
+    case REAL:
+      // TODO
+      break;
+    case DOUBLE:
+      // TODO
+      break;
+  }
+
+  return 0;
 }
 
-int neg_int(void) {
-	fprintf(obejct, "\tnegl %%eax\n");
-	return 0;
+// Binary instructions
+
+int add_instruction(int type) {
+  switch(type) {
+    case BOOLEAN:
+      // TODO
+      break;
+
+    case INTEGER:
+      fprintf(object, "\taddl %%rax, (%%rsp)\n");
+      fprintf(object, "\taddl $4, %%rsp\n");
+      break;
+
+    case REAL:
+      fprintf(object, "\tmovss (%%rsp), %%xmm1\n");
+      fprintf(object, "\tmovss %%rax, %%xmm0\n");
+      fprintf(object, "\taddss %%xmm1, %%xmm0\n");
+      fprintf(object, "\tmovss %%xmm0, %%rax\n");
+      fprintf(object, "\taddl $4, %%rsp\n");
+      break;
+
+    case DOUBLE:
+      fprintf(object, "\tmovsd (%%rsp), %%xmm1\n");
+      fprintf(object, "\tmovsd %%rax, %%xmm0\n");
+      fprintf(object, "\taddsd %%xmm1, %%xmm0\n");
+      fprintf(object, "\tmovsd %%xmm0, %%rax\n");
+      fprintf(object, "\taddq $8, %%rsp\n");
+      break;
+  }
+
+  return 0;
 }
 
-int neg_float(void) {
+int sub_instruction(int type) {
+  if (type == BOOLEAN) {
+    // TODO: throw an error or something like that
+    return -1;
+  }
 
+  switch(type) {
+    case INTEGER:
+      fprintf(object, "\tsubl %%rax, (%%rsp)\n");
+      fprintf(object, "\tpopl %%rax\n");
+      break;
+
+    case REAL:
+      fprintf(object, "\tmovss (%%rsp), %%xmm1\n");
+      fprintf(object, "\tmovss %%rax, %%xmm0\n");
+      fprintf(object, "\tsubss %%xmm1, %%xmm0\n");
+      fprintf(object, "\tmovss %%xmm0, %%rax\n");
+      fprintf(object, "\taddl $4, %%rsp\n");
+      break;
+
+    case DOUBLE:
+      fprintf(object, "\tmovsd (%%rsp), %%xmm1\n");
+      fprintf(object, "\tmovsd %%rax, %%xmm0\n");
+      fprintf(object, "\tsubsd %%xmm1, %%xmm0\n");
+      fprintf(object, "\tmovsd %%xmm0, %%rax\n");
+      fprintf(object, "\taddq $8, %%rsp\n");
+      break;
+
+  }
+
+  return 0;
 }
 
-int neg_double(void) {
+int mul_instruction(int type) {
+  switch(type) {
+    case BOOLEAN:
+      // TODO
+      break;
+    case INTEGER:
+      fprintf(object, "\tmull (%%rsp)\n");
+      fprintf(object, "\taddl $4, %%rsp\n");
+      break;
 
+    case REAL:
+      fprintf(object, "\tmovss (%%rsp), %%xmm1\n");
+      fprintf(object, "\tmovss %%rax, %%xmm0\n");
+      fprintf(object, "\tmulss %%xmm1, %%xmm0\n");
+      fprintf(object, "\tmovss %%xmm0, %%rax\n");
+      fprintf(object, "\taddl $4, %%rsp\n");
+      break;
+
+    case DOUBLE:
+      fprintf(object, "\tmovsd (%%rsp), %%xmm1\n");
+      fprintf(object, "\tmovsd %%rax, %%xmm0\n");
+      fprintf(object, "\tmulsd %%xmm1, %%xmm0\n");
+      fprintf(object, "\tmovsd %%xmm0, %%rax\n");
+      fprintf(object, "\taddq $8, %%rsp\n");
+      break;
+  }
+
+  return 0;
 }
 
-/*binary addition*/
-int add_logic(void) {
-	//OR
+int mod_instruction() {
+  fprintf(object, "divl (%%rsp)");
+  fprintf(obejct, "movl %%edx, %%rax");
+  return 0;
 }
 
-int add_int(void) {
-	fprintf(object, "\taddl %%eax, (%%esp)\n");
-	fprintf(object, "\taddl $4, %%esp\n"); //dec pilha
-	return 0;
-}
+int div_instruction(int type) {
+  if (type == BOOLEAN) {
+    // TODO: throw an error or something like that
+    return -1;
+  }
 
-int add_float(void) {
-	fprintf(object, "\tmovss (%%esp), %%xmm1\n");
-	fprintf(object, "\tmovss %%eax, %%xmm0\n");
-	fprintf(object, "\taddss %%xmm1, %%xmm0\n");
-	fprintf(object, "\tmovss %%xmm0, %%eax\n");
-	fprintf(object, "\taddl $4, %%esp\n"); //dec pilha
-	return 0;
-}
+  switch(type) {
+    case INTEGER:
+      fprintf(object, "\tdivl (%%rsp)\n");
+      fprintf(object, "\taddl $4, %%rsp\n");
+      break;
 
-int add_double(void) {
-	fprintf(object, "\tmovsd (%%rsp), %%xmm1\n");
-	fprintf(object, "\tmovsd %%rax, %%xmm0\n");
-	fprintf(object, "\taddsd %%xmm1, %%xmm0\n");
-	fprintf(object, "\tmovsd %%xmm0, %%rax\n");
-	fprintf(object, "\taddq $8, %%rsp\n"); //dec pilha
-	return 0;
-}
+    case REAL:
+      fprintf(object, "\tmovss (%%rsp), %%xmm0\n");
+      fprintf(object, "\tmovss %%rax, %%xmm1\n");
+      fprintf(object, "\tdivss %%xmm1, %%xmm0\n");
+      fprintf(object, "\tmovss %%xmm0, %%rax\n");
+      fprintf(object, "\taddq $8, %%rsp\n");
+      break;
 
-/*binary subtraction*/
-int sub_int(void) {
-	fprintf(object, "\tsubl %%eax, (%%esp)\n");
-	fprintf(object, "\tpopl %%eax\n");
-	return 0;
-}
+    case DOUBLE:
+      fprintf(object, "\tmovsd (%%rsp), %%xmm0\n");
+      fprintf(object, "\tmovsd %%rax, %%xmm1\n");
+      fprintf(object, "\tdivsd %%xmm1, %%xmm0\n");
+      fprintf(object, "\tmovsd %%xmm0, %%rax\n");
+      fprintf(object, "\taddq $8, %%rsp\n");
+      break;
+  }
 
-int sub_float(void) {
-	fprintf(object, "\tmovss (%%esp), %%xmm1\n");
-	fprintf(object, "\tmovss %%eax, %%xmm0\n");
-	fprintf(object, "\tsubss %%xmm1, %%xmm0\n");
-	fprintf(object, "\tmovss %%xmm0, %%eax\n");
-	fprintf(object, "\taddl $4, %%esp\n"); //dec pilha
-	return 0;
-}
-
-int sub_double(void) {
-	fprintf(object, "\tmovsd (%%rsp), %%xmm1\n");
-	fprintf(object, "\tmovsd %%rax, %%xmm0\n");
-	fprintf(object, "\tsubsd %%xmm1, %%xmm0\n");
-	fprintf(object, "\tmovsd %%xmm0, %%rax\n");
-	fprintf(object, "\taddq $8, %%rsp\n"); //dec pilha
-	return 0;
-}
-
-/*binary mutiplication*/
-int mul_logic(void) {
-	//AND
-}
-
-int mul_int(void) {
-	fprintf(object, "\tmull (%%esp)\n");
-	fprintf(object, "\taddl $4, %%esp\n");
-
-	return 0;
-}
-
-int mul_float(void) {
-	fprintf(object, "\tmovss (%%esp), %%xmm1\n");
-	fprintf(object, "\tmovss %%eax, %%xmm0\n");
-	fprintf(object, "\tmulss %%xmm1, %%xmm0\n");
-	fprintf(object, "\tmovss %%xmm0, %%eax\n");
-	fprintf(object, "\taddl $4, %%esp\n"); //dec pilha
-	return 0;
-}
-
-int mul_double(void) {
-	fprintf(object, "\tmovsd (%%rsp), %%xmm1\n");
-	fprintf(object, "\tmovsd %%rax, %%xmm0\n");
-	fprintf(object, "\tmulsd %%xmm1, %%xmm0\n");
-	fprintf(object, "\tmovsd %%xmm0, %%rax\n");
-	fprintf(object, "\taddq $8, %%rsp\n"); //dec pilha
-	return 0;
-}
-
-/*binary division*/
-
-int mod_int(void) {
-	fprintf(object, "divl (%%esp)");
-	fprintf(obejct, "movl %%edx, %%eax");
-	return 0;
-}
-
-int div_int(void) {
-	fprintf(object, "\tdivl (%%esp)\n");
-	fprintf(object, "\taddl $4, %%esp\n");
-	return 0;
-}
-
-int div_float(void) {
-	fprintf(object, "\tmovss (%%rsp), %%xmm0\n");
-	fprintf(object, "\tmovss %%rax, %%xmm1\n");
-	fprintf(object, "\tdivss %%xmm1, %%xmm0\n");
-	fprintf(object, "\tmovss %%xmm0, %%rax\n");
-	fprintf(object, "\taddq $8, %%rsp\n"); //dec pilha
-	return 0;
-}
-
-int div_double(void) {
-	fprintf(object, "\tmovsd (%%rsp), %%xmm0\n");
-	fprintf(object, "\tmovsd %%rax, %%xmm1\n");
-	fprintf(object, "\tdivsd %%xmm1, %%xmm0\n");
-	fprintf(object, "\tmovsd %%xmm0, %%rax\n");
-	fprintf(object, "\taddq $8, %%rsp\n"); //dec pilha
-	return 0;
+  return 0;
 }
--- pseudoassembly.h
+++ pseudoassembly.h
@@ -1,8 +1,8 @@
 
-/*global label counter*/
+// global label counter
 extern int labelcounter;
 
-/*control pseudo-instructions*/
+// control pseudo-instructions
 int go_false(int label);
 int jump(int label);
 int mklabel(int label);
@@ -11,32 +11,14 @@ int lmove_q(char const* variable);
 int rmove_int(char const* variable);
 int rmove_q(char const* variable);
 
-/*ULA pseudo-instructions*/
-/*unary*/
-int neg_logic(void);
-int neg_int(void);
-int neg_float(void);
-int neg_double(void);
+// ULA pseudo-instructions
 
-/*binary addition*/
-int add_logic(void);
-int add_int(void);
-int add_float(void);
-int add_double(void);
+// Unary instructions
+int neg_instruction(int type);
 
-/*binary subtraction*/
-int sub_int(void);
-int sub_float(void);
-int sub_double(void);
-
-/*binary mutiplication*/
-int mul_logic(void);
-int mul_int(void);
-int mul_float(void);
-int mul_double(void);
-
-/*binary division*/
-int div_int(void);
-int div_float(void);
-int div_double(void);
-
+// Binary Instructions
+int add_instruction(int type);
+int sub_instruction(int type);
+int mul_instruction(int type);
+int div_instruction(int type);
+int mod_instruction();
