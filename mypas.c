ea99ab9c10116535ecf86766316cbab02f64d19f
Lucas Pinheiro <pinheiro.lucasaugusto@gmail.com>
2016-11-30 01:23:34 -0200

    Refactor to use command line arguments

--- mypas.c
+++ mypas.c
@@ -1,33 +1,53 @@
-/**@<mypas.c>::**/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <parser.h>
 #include <mypas.h>
 
-FILE *source, *object;
+// Source file stream
+FILE *source;
+
+// Output file stream
+FILE *object;
+
+// Variable that will count the number of semantic errors in the source program
 int semanticErr = 0;
-extern int lookahead; // @ parser.c
 
-main (int argc, char *argv[], char *envp[])
-{
-	object = stdout;
-	/*if (argc == -1) {
-		source = stdin;
-	} else {*/
-		//source = fopen (argv[1], "r");
-		source = fopen("teste.txt", "r");
-	if (source == NULL) {
-		fprintf (stderr, "%s: cannot open %s... exiting\n",
-				argv[0], argv[1]);
-		exit (-1);
-	}
-	//}
+// @ parser.c
+extern int lookahead;
 
-	lookahead = gettoken (source);
+int main (int argc, char *argv[], char *envp[]) {
+  char* source_file_name = NULL;
+  char* output_file_name = NULL;
 
-	mypas();
+  if (argc == 2) {
+    source_file_name = argv[1];
+    output_file_name = "out.s";
+  } else if (argc == 4) {
+    if (!strcmp(argv[1], "-o")) {
+      output_file_name = argv[2];
+      source_file_name = argv[3];
+    } else if (!strcmp(argv[2], "-o")) {
+      source_file_name = argv[1];
+      output_file_name = argv[3];
+    } else {
+      fprintf(stderr, "[ERROR] Wrong passing of arguments!\n\n");
+      print_usage_message();
+      return -1;
+    }
+  } else {
+    print_usage_message();
+    return -1;
+  }
 
-	printf("\n");
-	exit (0);
+  source = fopen(source_file_name, "r");
+
+  if (source == NULL) {
+    fprintf(stderr, "%s: cannot open %s. Exiting...\n", argv[0], source_file_name);
+    return -2;
+  }
+
+  lookahead = gettoken(source);
+  mypas();
+
+  return semanticErr;
 }
